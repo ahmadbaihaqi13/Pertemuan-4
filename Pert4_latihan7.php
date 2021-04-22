71c029f5a465da6d3d682fcdc4693c4e0b398c92
ahmad baihaqi/181011402740 <82923706+ahmadbaihaqi13@users.noreply.github.com>
2021-04-22 20:50:31 +0700

    pertemuan 4

---
+++ Pert4_latihan7.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Penggunaan For</title>
+</head>
+<body>
+    <h1>Menghitung dari atas ke bawah </h1>
+<?Php
+ For ($bil = 1; $bil<25; $bil++)
+Print("$bil<br>\n");
+ ?>
+</body>
+</html> 
---
+++ Pert4_latihanE.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title><?php echo "Belajar PHP" ?></title>
+    </head>
+    <h1> Perbedaan Break dan Continue</h1>
+    <body>
+        <?php
+            echo "Break digunakan untuk mengakhiri sebuah eksekusi sebuah kontrol perulangan dan switch. 
+                  Kali ini kita akan mencoba untuk mengakhiri sebuah perulangan. 
+                  sedangkan Continue digunakan untuk melompati sebuah perulangan
+                  yang sudah ditentukan dan melanjutkan perulangan kembali<br>";
+        ?>
+    </body>
+</html>
